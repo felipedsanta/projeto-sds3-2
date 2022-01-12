7043e0fa1ccdd50de553406d04b3d86e01fcafc2
Felipe Santos <felipe.carro3@outlook.com>
2022-01-12 11:38:44 -0300

    Projeto criado

---
+++ backend/src/main/java/com/fsuperior/fsmovies/FsmoviesApplication.java
@@ -0,0 +1,12 @@
+package com.fsuperior.fsmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsmoviesApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/fsuperior/fsmovies/FsmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsuperior.fsmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
