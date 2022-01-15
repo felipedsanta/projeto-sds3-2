d8a35cbcde7fcf0a232ff3093b64288a5830d2b7
Felipe Santos <felipe.carro3@outlook.com>
2022-01-14 21:56:47 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/fsuperior/fsmovies/entidades/Movie.java
@@ -0,0 +1,108 @@
+package com.fsuperior.fsmovies.entidades;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+	
+
+public Movie ( ) {
+	
+}
+
+
+public Movie(Long id, String title, Double score, Integer count, String image) {
+	super();
+	this.id = id;
+	this.title = title;
+	this.score = score;
+	this.count = count;
+	this.image = image;
+}
+
+
+public Long getId() {
+	return id;
+}
+
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+
+public String getTitle() {
+	return title;
+}
+
+
+public void setTitle(String title) {
+	this.title = title;
+}
+
+
+public Double getScore() {
+	return score;
+}
+
+
+public void setScore(Double score) {
+	this.score = score;
+}
+
+
+public Integer getCount() {
+	return count;
+}
+
+
+public void setCount(Integer count) {
+	this.count = count;
+}
+
+
+public String getImage() {
+	return image;
+}
+
+
+public void setImage(String image) {
+	this.image = image;
+}
+
+
+@Override
+public int hashCode() {
+	return Objects.hash(id);
+}
+
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Movie other = (Movie) obj;
+	return Objects.equals(id, other.id);
+}
+
+
+}
---
+++ backend/src/main/java/com/fsuperior/fsmovies/entidades/User.java
@@ -0,0 +1,70 @@
+package com.fsuperior.fsmovies.entidades;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	
+	
+	public User() {
+	}
+
+
+	public User(Long id, String email) {
+		super();
+		this.id = id;
+		this.email = email;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
